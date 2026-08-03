46e5f0f6539f34ba7de889d2fd225170e8c53439
phothisai2015-art <phothisai2015@gmail.com>
2026-08-03 13:36:11 +0700

    lang.js

--- public/lang.js
+++ public/lang.js
@@ -599,9 +599,9 @@ var i18n = {
 	set_err_del_self: "คุณไม่สามารถลบตัวเองได้",
     set_confirm_del_user_title: "ยืนยันการลบพนักงาน",
     set_confirm_del_user_desc: "ต้องการลบพนักงานรหัส PIN: ",
-    set_log_action_init: "เริ่มต้นระบบ",
-    set_log_action_manage_staff: "จัดการพนักงาน",
-    set_log_action_system_setting: "ตั้งค่าระบบ",
+    set_log_action_init: "🚀 เริ่มต้นระบบ",
+    set_log_action_manage_staff: "👥 จัดการพนักงาน",
+    set_log_action_system_setting: "⚙️ ตั้งค่าระบบ",
     set_log_detail_init_success: "ตั้งค่า Admin คนแรกสำเร็จ",
     set_log_detail_del_staff: "ลบพนักงาน PIN: ",
     set_log_detail_add_staff: "เพิ่ม/แก้ไขพนักงาน PIN: ",
@@ -1208,16 +1208,16 @@ var i18n = {
 	set_err_del_self: "You cannot delete yourself.",
     set_confirm_del_user_title: "Confirm Deletion",
     set_confirm_del_user_desc: "Do you want to delete staff PIN: ",
-    set_log_action_init: "System Init",
-    set_log_action_manage_staff: "Manage Staff",
-    set_log_action_system_setting: "System Setting",
+    set_log_action_init: "🚀 System Init",
+    set_log_action_manage_staff: "👥 Manage Staff",
+    set_log_action_system_setting: "⚙️ System Setting",
     set_log_detail_init_success: "First Admin setup successful",
     set_log_detail_del_staff: "Deleted staff PIN: ",
     set_log_detail_add_staff: "Add/Edit staff PIN: ",
     set_log_detail_update_shop: "Updated store info & printing",
     set_log_detail_login_success: "Login successful",
     set_log_detail_logout_manual: "User locked screen (Manual)",
-    set_log_detail_logout_auto: "System auto-locked screen (Idle)"  
+    set_log_detail_logout_auto: "System auto-locked screen (Idle)"
   }
 };
 
